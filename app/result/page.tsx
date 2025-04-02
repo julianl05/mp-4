18fd1ce3ee4993b36f23843fe403ad1484711183
Julian Lim <limjunre@bu.edu>
2025-04-02 00:55:07 -0400

    added loading and error states to results

--- app/result/page.tsx
+++ app/result/page.tsx
@@ -1,21 +1,53 @@
 "use client";
 import { use, useEffect, useState } from "react";
 import { getSuperheroData } from "@/lib/getSuperheroData";
+import { Superhero } from "@/types";
 export default function ResultsPage(){
-    const [superheroData, setSuperheroData] = useState(null);
+    const [superheroData, setSuperheroData] = useState<Superhero[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string | null>(null);
     useEffect(() => {
         getSuperheroData()
             .then((data)=>{
                     console.log("Data fetched Successfully");
                     console.log(data);
-                    setSuperheroData(data);
+                    if (data.response === "error") {
+                        setError(data.error);
+                    } else {
+                        setSuperheroData(data.results);
+                    }
+                    setLoading(false);
                 })
             .catch((e: Error)=>console.log("There was an error fetching data", e));
-    },[]);
+    },[setSuperheroData]);
+
+    if (error) {
+        return <p>{error}</p>;
+    }
+
     return(
         <>
             <h1>Superhero Data</h1>
-            <p>{JSON.stringify(superheroData, null, 2)}</p>
+            <h2>Results for:</h2>
+            <div>
+                { loading ? <p>Loading...</p> :
+                    superheroData.map((superhero, index)=>(
+                        <div key={index}>
+                            <h2>{superhero.name}</h2>
+                            <img src={superhero.image.url} alt={superhero.name} />
+                            <h3>Powerstats</h3>
+                            <ul>
+                                <li>Intelligence: {superhero.powerstats.intelligence}</li>
+                                <li>Strength: {superhero.powerstats.strength}</li>
+                                <li>Speed: {superhero.powerstats.speed}</li>
+                                <li>Durability: {superhero.powerstats.durability}</li>
+                                <li>Power: {superhero.powerstats.power}</li>
+                                <li>Combat: {superhero.powerstats.combat}</li>
+                            </ul>
+                        </div>
+                    ))
+                }
+            </div>
         </>
     )
 }
--- lib/getSuperheroData.ts
+++ lib/getSuperheroData.ts
@@ -1,6 +1,6 @@
 "use server";
 export async function getSuperheroData() {
-    const res = await fetch(`https://superheroapi.com/api/${process.env.SUPERHERO_API_KEY}/search/asdas`);
+    const res = await fetch(`https://superheroapi.com/api/${process.env.SUPERHERO_API_KEY}/search/superman`);
     const data = await res.json();
     console.log(data);
     return data;
