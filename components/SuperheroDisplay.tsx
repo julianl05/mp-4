74a7e1177a64d7345db9609290cb43e553c2d074
Julian Lim <limjunre@bu.edu>
2025-04-03 19:01:20 -0400

    fixed styling for unavailable images and text

--- components/SuperheroDisplay.tsx
+++ components/SuperheroDisplay.tsx
@@ -3,13 +3,13 @@ import { Superhero } from "@/types";
 export default function SuperheroDisplay(props: {data: Superhero[]}) {
     const powerstatStyling= "m-[1vw] p-[1vw] bg-[#535C91] rounded-lg";
     return(
-        <div className="flex flex-row justify-center items-center flex-wrap w-full">
+        <div className="flex flex-row justify-center items-center flex-wrap w-full break-words"> {/*break-words enables text to linebreak to prevent overflow*/}
             {props.data.map((superhero: Superhero, index: number)=>(
                 <div key={index} className="flex flex-col justify-center items-center m-[1vw] p-[2vw] rounded-lg w-[55vw] bg-[#31363F] shadow-lg">
                     <h2 className="font-bold text-[calc(5px+1vw)]">{superhero.name}</h2>
                     <h3>Real Name: {superhero.biography["full-name"] != "" ? superhero.biography["full-name"] : "Unavailable"}</h3>
-                    <div className="flex flex-row justify-center items-center">
-                        <img className="w-[45%]" src={superhero.image.url} alt={superhero.name} />
+                    <div className="flex flex-row justify-center items-center w-full">
+                        <img className="w-[45%] border-2" src={superhero.image.url} alt={superhero.name} />
                         <ul className="list-none p-0 m-[2vw] text-center w-[35%]">
                             <li className={powerstatStyling}>Intelligence: {superhero.powerstats.intelligence != "null" ? superhero.powerstats.intelligence : "Unavailable"}</li>
                             <li className={powerstatStyling}>Strength: {superhero.powerstats.strength != "null" ? superhero.powerstats.strength : "Unavailable"}</li>
